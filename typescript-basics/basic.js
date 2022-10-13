4f4c9c7b5e4680edb0e81d313c26e265f5121ab4
sriman <sriman.a@credright.com>
2022-10-13 16:34:25 +0530

    basics

---
+++ typescript-basics/basic.js
@@ -0,0 +1,15 @@
+"use strict";
+// TypeScript is a superSet of javaScript.
+// --> It is mainly focosed on static type checking(data type checking)
+// --> It supports ES6 , class based objects
+// --> It is readability and predectability.
+// Browser can't read the typeScrit, we need convert to javaScript.
+// cross-platform and cross campatability(aslo support mobile applications)
+// More ever typeScript is not new language,it is extenstion of javascript.
+// typeScript Type==>
+// -->string,number,boolean,any    
+// -->objects,arrays,void,null,undefined,tuples,enums,generics,interfaces,costom types
+// Type inferrence :-->It shows about what dataType is having variable.
+// Type Assigment :--> assigning dataType to variable
+let somename = "MANU";
+somename = "sriman"; // can't assign number to somename
